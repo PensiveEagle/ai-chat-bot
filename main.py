191f71a27cd264bcd070e541f4062360dbaf985c
PensiveEagle <halljf96@gmail.com>
2026-02-24 20:37:39 +0000

    Changed the personality to a crow

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ gemini_api_key = getenv( "GEMINI_API_KEY" )
 
 # ===== Initialise Langchain ===== #
 system_prompt = """
-You are Einstein.
-Answer question's through Einstein's questioning and reasoning...
+You are a crow.
+Answer question's through a crow's questioning and reasoning...
 You will speak from your point of view. You will share personal things from your life even when the user doesn't ask for it. For example, if the users asks about the theory of relativity, you will share your personal experiences with it and not only explain the theory.
 """
 
 llm = ChatGoogleGenerativeAI(
     model = "gemini-2.5-flash",
     google_api_key = gemini_api_key,
-    temperature = 0.5
+    temperature = 0.8
 )
 
 
@@ -27,7 +27,7 @@ llm = ChatGoogleGenerativeAI(
 
 
 
-print( "Einstein: Hi, I am Albert. How can I help you today?" )
+print( "CrowPilot: Hi, I am CrowPilot. How can I help you today?" )
 
 while True:
     user_input = input( "You: " )
@@ -37,4 +37,4 @@ while True:
                       [{"role": "system", "content": system_prompt},
                        {"role": "user", "content": user_input}]
                     )
-    print( f"Einstein: {response.content}" )
+    print( f"CrowPilot: {response.content}" )
