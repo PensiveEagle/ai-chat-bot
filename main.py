6ef8f3fd5e4dcfd1da05b929f447723d7b7b5be4
PensiveEagle <halljf96@gmail.com>
2026-03-12 20:47:32 +0000

    Added gradio web interface

--- main.py
+++ main.py
@@ -6,14 +6,33 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
 
+import gradio as gr
+
 # ===== Get keys ===== #
 load_dotenv()
 gemini_api_key = getenv( "GEMINI_API_KEY" )
 
+# ===== Functions ===== #
+def chat( user_in, hist ):
+    langchain_history = []
+    
+    for item in hist:
+        if item["role"] == "user":
+            langchain_history.append( HumanMessage( content = item["content"] ) )
+        elif item["role"] == "assistant":
+            langchain_history.append( AIMessage( content = item["content"] ) ) 
+        
+    response = chain.invoke({"input": user_in, "history": langchain_history})
+    
+    return "", hist + [{"role": "user", "content": user_in},
+                {"role": "assistant", "content": response}]
+
+def clear_chat():
+    return "", []
 
 # ===== Initialise Langchain ===== #
-bot_name = "CrowPilot"
-bot_personanlity = "a crow"
+bot_name = "BarkBot"
+bot_personanlity = "a dog"
 
 system_prompt = f"""
 You are {bot_personanlity}. Your name is {bot_name}.
@@ -36,16 +55,29 @@ prompt = ChatPromptTemplate.from_messages([
 
 chain = prompt | llm | StrOutputParser()
 
-history = []
+# ===== Interface ===== #
+page = gr.Blocks(
+    title = f"Chat with {bot_name}",
+    theme = gr.themes.Soft()
+)
 
-# ========== #
-print( f"{bot_name}: Hi, I am {bot_name}. How can I help you today?" )
-
-while True:
-    user_input = input( "You: " )
-    if user_input == "stop":
-        break
-    response = chain.invoke( {"input": user_input, "history": history} )
-    print( f"{bot_name}: {response}" )
-    history.append( HumanMessage( content = user_input ) )
-    history.append( AIMessage( content = response ) )
+with page:
+    gr.Markdown(
+        f"""
+        # Chat with {bot_name}
+        Welcome to your personal chat with {bot_name}!
+        """
+    )
+    
+    chatbot = gr.Chatbot(show_label = False)
+    
+    msg = gr.Textbox(show_label = False, placeholder = f"Enter your message to {bot_name}...")
+    
+    msg.submit( chat, [msg, chatbot], [msg, chatbot] )
+    
+    clear = gr.Button("Clear chat", variant = "secondary" )
+    clear.click(clear_chat, outputs=[msg, chatbot])
+    
+    
+# ===== Launch ===== #
+page.launch(share = True)
